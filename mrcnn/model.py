9b6c6f061f0d20def636eed8ddad705c6e2c12ab
prffraia <124781832+prffraia@users.noreply.github.com>
2023-07-27 10:52:55 +0200

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1872,8 +1872,8 @@ class MaskRCNN():
             input_gt_boxes = KL.Input(
                 shape=[None, 4], name="input_gt_boxes", dtype=tf.float32)
             # Normalize coordinates
-            gt_boxes = KL.Lambda(lambda x: norm_boxes_graph(
-                x, K.shape(input_image)[1:3]))(input_gt_boxes)
+            gt_boxes = KL.Lambda(lambda x: norm_boxes_graph2(
+                x))([input_gt_boxes, input_image]) 
             # 3. GT Masks (zero padded)
             # [batch, height, width, MAX_GT_INSTANCES]
             if config.USE_MINI_MASK:
